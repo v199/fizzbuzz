ca9ac0f3095cab697d979f8846d0537dfe9e0215
Vinay D <vpd5017@psu.edu>
2016-06-28 13:56:51 -0400

    fizzbuzz project completed

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	for(var i=1;i<=100;i++){
+		if(i%3 == 0){
+			console.log('fizz');
+		}
+		if(i%5 == 0){
+			console.log('buzz');
+		}
+		if(i%15 == 0){
+			console.log('fizzbuzz');
+		}
+		else{
+			console.log(i);
+		}
+	}
+});
